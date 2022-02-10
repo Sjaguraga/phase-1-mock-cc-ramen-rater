c6c2fc05fc8428961b786027ff218e447eb04d72
serref jaguraga <reef163@gmail.com>
2022-02-10 11:14:11 -0500

    commit tree 3e23e540f71ca0995dc755b1e11140d3ea397eaa

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-// write your code here
+let ramenImg = document.createElement('img')
+let ramenDiv = document.getElementById('ramen-menu')
+let ramenName = document.querySelector('.name')
+let ramenRest = document.querySelector('.restaurant')
+let ramenForm = document.getElementById('new-ramen')
+let ramenRating = document.getElementById('ramen-detail')
+
+
+fetch(`http://localhost:3000/ramens/`)
+.then(resp => resp.json())
+.then(data => console.log(data))
+
+
+//fetch
+// fetch('http://localhost:3000/ramens/:id')
+// .then(resp => resp.json())
+// .then(data => console.log(data))
+
+
+
+function ramenInfo(){
+ramenDiv.append(ramenImg, ramenName, ramenRating)
+}
+
+ramenInfo();
+
+function ramenDetails(ramen){
+ramenImg.src = ramen.image
+ramenName.textContent = ramen.name
+ramenDetails()
+}
+
+ramenForm.addEventListener("click", function(event){
+    let p = document.createElement("p")
+   ramenForm.append(p)
+event.preventDefault()
+})
